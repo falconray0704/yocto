b25b24b084867810603f03286af1709cdbdb83f9
FalconRay <falconray@yahoo.com>
2019-12-17 18:14:35 +0800

    Refine tips of get resource.

--- tips.sh
+++ tips.sh
@@ -15,6 +15,25 @@ get_resources_func()
     echoC "Get Yocto release:"
     echo "http://downloads.yoctoproject.org/releases/"
     echo ""
+    echoC "Get Yocto cookbook 2nd codes:"
+    echo "https://github.com/PacktPublishing/Embedded-Linux-Development-Using-Yocto-Proje
+ct-Cookbook-Second-Edition"
+    echo "https://github.com/PacktPublishing/"
+    echo "https://github.com/yoctocookbook2ndedition"
+    echo ""
+    echoC "Get poky:"
+    echo "http://git.yoctoproject.org/cgit/cgit.cgi/poky/"
+    echo ""
+    echoC "Get bitbake:"
+    echo "http://git.openembedded.org/bitbake/"
+    echo ""
+    echoC "Get OpenEmbedded-core metadata:"
+    echo "http://git.openembedded.org/openembedded-core/"
+    echo ""
+    echoC "Get official Yocto Project documentation:"
+    echo " http://www.yoctoproject.org/docs/"
+    echo ""
+
 }
 
 tips_help_func()
